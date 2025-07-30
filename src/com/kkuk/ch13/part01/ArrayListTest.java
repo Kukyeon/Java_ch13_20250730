aca9df0b369bd56160a9d992f48700d70209e561
Kukyeon <rnrdus2000@naver.com>
2025-07-30 14:26:08 +0900

    ArrayList 예제 추가

---
+++ src/com/kkuk/ch13/part01/ArrayListTest.java
@@ -0,0 +1,29 @@
+package com.kkuk.ch13.part01;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		List<String> strList = new ArrayList<String>(); // 객체를 저장할수있는 박스 생성
+		
+		strList.add("Kor");
+		strList.add("Jap"); //(string) 인덱스 0 부터 순차 적용
+		strList.add(1, "USA");  // (index , string) 1번 인덱스에 usa 를 삽입
+		
+		System.out.println(strList.get(0));
+		System.out.println(strList.get(1));
+		System.out.println(strList.get(2));
+		
+		System.out.println("===================================");
+		strList.remove(1); // usa 인덱스 1 , / 정해진 인덱스값에 내용을 빼줌
+		
+		System.out.println(strList.get(0)); // kor
+		System.out.println(strList.get(1));	// jap
+		System.out.println(strList.get(2)); // 비어있음 에러발생
+	}
+
+}
