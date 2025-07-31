a317ceb10cc3e9435a10c6d1eba5c8afb9bc2738
Kukyeon <rnrdus2000@naver.com>
2025-07-31 10:32:42 +0900

    향상된 for문 주석 추가

--- src/exex/MemberMain.java
+++ src/exex/MemberMain.java
@@ -14,6 +14,12 @@ public class MemberMain {
 		List<MemberDto> dto = member.getMemberList();
 		
 		
+		// iterator 와 while 을 사용하지않고 향상된 for 문 으로 사용 시
+//		System.out.println("아이디 / 비밀번호 / 이름 / 이메일");
+//        for (MemberDto mem : dto) {
+//            System.out.println(mem.getId() + "/" + mem.getPw() + "/" + mem.getName() + "/" + mem.getEmail());
+		
+		
 		Iterator<MemberDto> iter = dto.iterator();
 		
 		while(iter.hasNext()) {
